7c5eda61b2627728f79c8182121a35bf42631e34
Marcin Kordus <marcin.kordus8@gmail.com>
2015-10-11 10:34:03 +0200

    wip

---
+++ autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+setxkbmap -option ctrl:nocaps
+xcape -e '#66=Escape'
+xset r rate 180 40
+
+xrdb -load ~/dotfiles/Xresources
+xrdb -merge ~/dotfiles/Xresources-luciusLightLowContrast &
+
+if xrandr | grep -q 'HDMI1 connected'; then
+    xrandr --output LVDS1 --off
+fi
