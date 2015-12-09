16444f15c59d6549c6d98d320aaf8715e923095a
Marcin Kordus <marcin.kordus@edpauto.com>
2015-12-09 17:11:21 +0100

    wip

--- colorize_phpunit.sh
+++ colorize_phpunit.sh
@@ -6,6 +6,7 @@ MAGENTA=`echo -e '\033[35m'`
 BLUE=`echo -e '\033[34m'`
 YELLOW=`echo -e '\033[33m'`
 CYAN=`echo -e '\033[36m'`
+UNDERLINE=`echo -e '\033[4m'`
 NORMAL=`echo -e '\033[0m'`
 CWD=`pwd`
 
@@ -15,17 +16,20 @@ tee \
     | sed "/^PHPUnit /,+1d" \
     | sed "/^Configuration read from /,+1d" \
     | sed "/^There were [0-9]\+ errors:/,+1d" \
+    | sed "/^There was 1 error:/,+1d" \
     | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" \
-    | sed -e "s#$CWD#.#" \
-    | sed -e "s#\.\?\(/[A-Za-z.-]\+\)#${MAGENTA}&${NORMAL}#g" \
-    | sed -e "s#\([A-Za-z.-]\+[\\]\)\+[A-Za-z.-]\+#${BLUE}&${NORMAL}#g" \
-    | sed -e "s/\(instance of \)\([^ ]\+\)\( given,\)/\1${BLUE}\2${NORMAL}\3/g" \
-    | sed -e "s#::\$\?\w\+\(()\)\?#${CYAN}&${NORMAL}#g" \
-    | sed -e "s#\(on line \|:\)\([0-9]\+\)#\1${RED}\2${NORMAL}#g" \
-    | sed -e "s/^[E.]\+$/${RED}&${NORMAL}/" \
+    | sed -e "s#$CWD#.#g" \
+    | sed -e "s#\./src\(/[A-Za-z.-]\+\)\+#${UNDERLINE}&${NORMAL}#g" \
+    | sed -e "s#\(::\|->\)\(\$\?\w\+\(()\)\?\)#\1${BLUE}\2${NORMAL}#g" \
+    | sed -e "s#\(on line \|:\)\([0-9]\+\)#\1${YELLOW}\2${NORMAL}#g" \
     | sed -e "s#^Catchable fatal error: #${RED}&${NORMAL}#" \
+    | sed -e "s#^Call Stack:#${RED}&${NORMAL}#" \
     | sed -e "s#^Fatal error: #${RED}&${NORMAL}#" \
     | sed -e "s#^Notice: #${RED}&${NORMAL}#" \
     | sed -e "s#^BadMethodCallException: #${RED}&${NORMAL}#" \
     | sed -e "s#^FAILURES!#${RED}&${NORMAL}#" \
-    | sed -e "s/^OK (.*$/${GREEN}&${NORMAL}/"
+    | sed -e "s/^OK (.*$/${GREEN}&${NORMAL}/" \
+    | sed -e "s#\([A-Za-z.]\+[\\]\)\+[A-Za-z.]\+#${GREEN}&${NORMAL}#g" \
+    | sed -e "s/\(instance of \)\([^ ]\+\)\( given,\)/\1${GREEN}\2${NORMAL}\3/g" \
+    | sed -e "s/^[0-9. ]\{21\}//g"
+
