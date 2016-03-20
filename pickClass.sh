0e724e90ce1bf5823dfd562dd0b2f379d3cfc987
Marcin Kordus <marcin.kordus8@gmail.com>
2016-03-20 20:12:01 +0100

    wip

--- pickClass.sh
+++ pickClass.sh
@@ -9,17 +9,20 @@ CYAN=`echo -e '\033[36m'`
 UNDERLINE=`echo -e '\033[4m'`
 NORMAL=`echo -e '\033[0m'`
 
-cat tags \
+tee \
     | grep '\(c\|i\)$' \
+    | sed -n '/\/\^\s*\(class\|interface\|final\|abstract\)/p' \
     | sed 's#^[^\t]*\t\([^\t]\+\)\t/^\s*\([^$]\+\)\$/;"\s*\(\w\)$#\2\t\1#' \
     | sed 's#^abstract class#a #I' \
+    | sed 's#^final class#f #I' \
     | sed 's#^class#c #I' \
     | sed 's#^interface#i #I' \
     | sed 's# implements # im #gI' \
     | sed 's# extends # ex #gI' \
-    | column -t -s $'\t' \
+    | sed 's#\\\\#\\#g' \
+    | sed 's#\t#          #g' \
     | sed "s# ex # ${YELLOW}ex${NORMAL} #g" \
-    | sed "s# im # ${BLUE}im${NORMAL} #g" \
-    | fzf -e --ansi
+    | sed "s# im # ${GREEN}im${NORMAL} #g" \
+    | sed "s#^\(\w\s\+\)\(\w\+\)#\1${BLUE}\2${NORMAL}#"
 
-
+    # | column -t -s $'\t' \
